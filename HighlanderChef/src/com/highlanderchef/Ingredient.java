8e2802c8d2a6be286c592ee2c8c232631e1c1d9f
nick543219876 <nbarr007@ucr.edu>
2014-11-05 19:44:26 -0800

    added to ingredient and recipe classes

--- HighlanderChef/src/com/highlanderchef/Ingredient.java
+++ HighlanderChef/src/com/highlanderchef/Ingredient.java
@@ -2,8 +2,14 @@ package com.highlanderchef;
 
 public class Ingredient {
 	String name;
-	
-	double amount;
-	
-	// Picture?
+	String amount; //ex. 3, 1/2 cup, 5 ounces.
+
+	public String getName(){ return this.name; }
+	public void setName(String n){this.name = n;}
+
+	public String getAmount(){return this.amount; }
+	public void setAmount(String a){this.amount = a; }
 }
+
+// Picture?
+
--- HighlanderChef/src/com/highlanderchef/Recipe.java
+++ HighlanderChef/src/com/highlanderchef/Recipe.java
@@ -13,4 +13,23 @@ public class Recipe {
 	ArrayList<Ingredient> ingredients;
 	ArrayList<Direction> directions;
 
+	public void setIngredients(ArrayList<Ingredient> i)
+	{
+		this.ingredients = i;
+	}
+	public ArrayList<Ingredient> getIngredients()
+	{
+		return this.ingredients;
+	}
+	public void setAnIngredient(int index, Ingredient i)
+	{
+		this.ingredients.set(index, i);
+	}
+	public Ingredient getAnIngredient(int index)
+	{
+		return this.ingredients.get(index);
+	}
+
+
+
 }
