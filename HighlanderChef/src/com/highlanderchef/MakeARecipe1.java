b70de524735818a3a371584401dc25678095fddb
nick543219876 <nbarr007@ucr.edu>
2014-11-11 21:31:13 -0800

    add main image

--- HighlanderChef/src/com/highlanderchef/MakeARecipe1.java
+++ HighlanderChef/src/com/highlanderchef/MakeARecipe1.java
@@ -1,12 +1,18 @@
 package com.highlanderchef;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MakeARecipe1 extends ActionBarActivity {
 	Recipe recipe = new Recipe();
@@ -25,7 +31,6 @@ public class MakeARecipe1 extends ActionBarActivity {
 	}
 	public void AddIngrediantPressed(View view)
 	{
-
 		EditText edittext_name = (EditText) findViewById(R.id.recipe_title);
 		String new_name = edittext_name.getText().toString();
 
@@ -46,6 +51,64 @@ public class MakeARecipe1 extends ActionBarActivity {
 		Intent intent = new Intent(this, MakeARecipe2.class);
 		intent.putExtra("recipe", recipe);
 		startActivity(intent);
+	}
+	private static int RESULT_LOAD_IMAGE = 1;
+	public void AddImagePressed(View view)
+	{
+		Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		startActivityForResult(i, RESULT_LOAD_IMAGE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null)
+		{
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+			Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+			cursor.moveToFirst();
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+
+			ImageView imageView = (ImageView) findViewById(R.id.added_image);
+
+			//Get the dimensions of the Image View
+			int targetW = imageView.getWidth();
+			int targetH = imageView.getHeight();
+
+			//load bitmap
+			BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+			bmOptions.inJustDecodeBounds = true;
+			BitmapFactory.decodeFile(picturePath, bmOptions);
+
+			//Get the dimensions of the bitmap
+			int photoW = bmOptions.outWidth;
+			int photoH = bmOptions.outHeight;
+
+			//scale image
+			int scalefactor = Math.min(photoW/targetW, photoH/targetH);
+
+			// Decode the image file into a Bitmap sized to fill the View
+			bmOptions.inJustDecodeBounds = false;
+			bmOptions.inSampleSize = scalefactor;
+			bmOptions.inPurgeable = true;
+
+			Bitmap bitmap = BitmapFactory.decodeFile(picturePath, bmOptions);
+
+			if(bitmap != null)
+			{
+				imageView.setImageBitmap(bitmap);
+				recipe.mainImagepath = picturePath;
+			}
+			else
+			{
+				//TODO added error response
+			}
+		}
 	}
 
 	@Override
