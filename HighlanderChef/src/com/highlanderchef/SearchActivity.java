c5f1d00e84b1942cc8198e86eabf9789f5633a4b
nick543219876 <nbarr007@ucr.edu>
2014-11-12 22:53:42 -0800

    uncommented stuff

--- HighlanderChef/src/com/highlanderchef/SearchActivity.java
+++ HighlanderChef/src/com/highlanderchef/SearchActivity.java
@@ -58,6 +58,8 @@ public class SearchActivity extends ActionBarActivity {
 		}
 		for(int i = 0; i < recipies.size(); ++i)
 		{
+			if(recipies.get(i) == null )
+				continue;
 			if(i >= 1)
 			{
 				ImageView iv_divider = new ImageView(this);
@@ -84,18 +86,17 @@ public class SearchActivity extends ActionBarActivity {
 
 				iv_divider.setImageBitmap(div);
 				rl.addView(iv_divider);
-
 			}
 			TextView tv = new TextView(this);
 			tv.setText(recipies.get(i).getName());
 			//tv.setPadding(0, (i * 30), 0, 0);
 			rl.addView(tv);
-			/*if(recipies.get(i).isMainImage())
+			if(recipies.get(i).isMainImage())
 			{
 				ImageView iv = new ImageView(this);
 				iv.setImageBitmap(recipies.get(i).getMainImage());
 				rl.addView(iv);
-			}*/
+			}
 			TextView tv_descr = new TextView(this);
 			tv_descr.setText(recipies.get(i).getDescription());
 			rl.addView(tv_descr);
