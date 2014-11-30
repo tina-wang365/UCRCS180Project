22c324bb1668c341f22438226d9457a6b3ec4eb7
tipsyTentacle <tipsyTentacle@users.noreply.github.com>
2014-11-29 20:11:38 -0800

    Homepage can view recipes

--- HighlanderChef/src/com/highlanderchef/UserHomepage.java
+++ HighlanderChef/src/com/highlanderchef/UserHomepage.java
@@ -12,14 +12,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class UserHomepage extends ActionBarActivity {
 
 	//User CurrentUser;
+	//User UserBeingViewed
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +53,6 @@ public class UserHomepage extends ActionBarActivity {
 		//Add current user to the user's (being viewed) follower list
 	}
 
-	public void callRecipeIntent(int index)
-	{
-		Intent intent = new Intent(this, RecipeForum.class);
-		intent.putExtra("recipeID", index);
-		startActivity(intent);
-	}
-
 	public void setUsername(String iName)
 	{
 		((TextView) findViewById(R.id.Username)).setText(iName);
@@ -91,16 +84,20 @@ public class UserHomepage extends ActionBarActivity {
 
 	public void PopulateRecipeList(ArrayList<Recipe> RecipeList)
 	{
-		LinearLayout ILinearLayout = (LinearLayout) findViewById(R.id.RecipeLayout);
+		//debug
+		RelativeLayout ILayout = (RelativeLayout) findViewById(R.id.RelativeLayout1);
+		ID_Maker MakerInstance = ID_Maker.getInstance();
+		View lastView = null;
 		for(int i = 0; i < RecipeList.size(); ++i)
 		{
 			if(RecipeList.get(i) == null )
 				continue;
 			//code for dividers
+
+			ImageView iv_divider = new ImageView(this);
 			if(i >= 1)
 			{
-
-				ImageView iv_divider = new ImageView(this);
+				iv_divider.setId(MakerInstance.useCurrID());
 
 				BitmapFactory.Options bmOptions = new BitmapFactory.Options();
 				bmOptions.inJustDecodeBounds = true;
@@ -123,43 +120,95 @@ public class UserHomepage extends ActionBarActivity {
 				Bitmap div = BitmapFactory.decodeResource(getResources(), R.drawable.divider, bmOptions);
 
 				iv_divider.setImageBitmap(div);
-				ILinearLayout.addView(iv_divider);
+				RelativeLayout.LayoutParams ivdParams =
+						new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+				ivdParams.addRule(RelativeLayout.BELOW, lastView.getId());
+				ivdParams.setMargins(0, 5, 0, 5);
+				ILayout.addView(iv_divider, ivdParams);
 			}
+
 			TextView tv = new TextView(this);
-			tv.setText(RecipeList.get(i).getName() + "\n");
-			//tv.setPadding(0, (i * 30), 0, 0);
-			ILinearLayout.addView(tv);
+			tv.setId(MakerInstance.useCurrID());
+			tv.setText(RecipeList.get(i).getName());
+			RelativeLayout.LayoutParams tParams =
+					new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			if (ILayout.getChildCount() > 0)
+				tParams.addRule(RelativeLayout.BELOW, iv_divider.getId());
+			tParams.setMargins(0, 5, 0, 5);
+			tv.setLayoutParams(tParams);
+			ILayout.addView(tv);
+
+			ImageView iv = new ImageView(this);
+			iv.setId(MakerInstance.useCurrID());
 			if(RecipeList.get(i).isMainImage())
 			{
-				ImageView iv = new ImageView(this);
-				iv.setImageBitmap(RecipeList.get(i).getMainImage());
-				ILinearLayout.addView(iv);
+				iv.setImageBitmap(Bitmap.createScaledBitmap(RecipeList.get(i).getMainImage(), 100, 100, false));
 			}
-			TextView tv_descr = new TextView(this);
-			tv_descr.setText("Description \n" + RecipeList.get(i).getDescription());
-			ILinearLayout.addView(tv_descr);
+			RelativeLayout.LayoutParams ivParams =
+					new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			ivParams.addRule(RelativeLayout.BELOW, tv.getId());
+			ivParams.addRule(RelativeLayout.ALIGN_LEFT, tv.getId());
+			if (i == 0)
+				ivParams.setMargins(0, 35, 10, 5);
+			else
+				ivParams.setMargins(0, 5, 10, 5);
+			iv.setLayoutParams(ivParams);
+			ILayout.addView(iv);
+			lastView = iv;
 
-			TextView tv_cooktime = new TextView(this);
-			tv_cooktime.setText(RecipeList.get(i).getCookTime());
-			ILinearLayout.addView(tv_cooktime);
+			TextView tv_descr = new TextView(this);
+			tv_descr.setId(MakerInstance.useCurrID());
+			String description = "Description: " + RecipeList.get(i).getDescription();
+			if (description.length() >= 100)
+				tv_descr.setText(description.substring(0,100) + "...");
+			else
+				tv_descr.setText(description);
+			RelativeLayout.LayoutParams dParams =
+					new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			dParams.addRule(RelativeLayout.RIGHT_OF, iv.getId());
+			dParams.addRule(RelativeLayout.ALIGN_TOP, iv.getId());
+			dParams.setMargins(0, 5, 0, 5);
+			ILayout.addView(tv_descr, dParams);
+
+
+			//			TextView tv_cooktime = new TextView(this);
+			//			tv_cooktime.setText(RecipeList.get(i).getCookTime());
+			//			RelativeLayout.LayoutParams cParams =
+			//					new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			//			cParams.addRule(RelativeLayout.BELOW, tv_descr.getId());
+			//			cParams.addRule(RelativeLayout.ALIGN_LEFT, tv_descr.getId());
+			//			ILayout.addView(tv_cooktime);
 
 			final int j = RecipeList.get(i).id; //so java doesn't complain
 
-			Button b_view = new Button(this);
-			b_view.setText("View");
-
-			b_view.setOnClickListener(new View.OnClickListener(){
-				@Override
-				public void onClick(View v)
-				{
-
-					callRecipeIntent(j);
-				}
-			});
-			ILinearLayout.addView(b_view);
+			//			Button b_view = new Button(this);
+			//			b_view.setId(MakerInstance.useCurrID());
+			//			b_view.setText("View");
+			//
+			//			b_view.setOnClickListener(new View.OnClickListener(){
+			//				@Override
+			//				public void onClick(View v)
+			//				{
+			//
+			//					callRecipeIntent(j);
+			//				}
+			//			});
+			//			RelativeLayout.LayoutParams bParams =
+			//					new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			//			bParams.addRule(RelativeLayout.ALIGN_TOP, tv_cooktime.getId());
+			//			bParams.addRule(RelativeLayout.RIGHT_OF, tv_cooktime.getId());
+			//			bParams.setMargins(0, 5, 0, 5);
+			//			ILayout.addView(b_view, bParams);
+			//			lastView = b_view;
 		}
 	}
 
+	public void callRecipeIntent(int index)
+	{
+		Intent intent = new Intent(this, RecipeForum.class);
+		intent.putExtra("recipeID", index);
+		startActivity(intent);
+	}
 
 	@SuppressWarnings("unused")
 	private class UserRecipes extends AsyncTask<String, Void, Boolean>
@@ -171,7 +220,7 @@ public class UserHomepage extends ActionBarActivity {
 			Comm IComm = new Comm();
 			//Get Recipes of the User
 			UserRecipeList = IComm.searchRecipesByUID(0);
-			return (UserRecipeList.size() > 0);
+			return (UserRecipeList != null);
 		}
 
 
@@ -180,14 +229,12 @@ public class UserHomepage extends ActionBarActivity {
 		{
 			if (result != true)
 			{
-				//log error
-				//Make empty list
+				Log.e("LOAD_RECIPE", "Failed to load user's recipes");
+				UserRecipeList = new ArrayList<Recipe>();
 				//Display "User has no recipes"
 			}
-			//Goto function that populate page with recipes;
+			PopulateRecipeList(UserRecipeList);
 		}
 
 	}
 }
-
-
