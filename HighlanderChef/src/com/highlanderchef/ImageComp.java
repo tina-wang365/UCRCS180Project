9b8eb3030372c5d64f21155073df6a5861baaa25
nick543219876 <nbarr007@ucr.edu>
2014-12-03 18:58:05 -0800

    started adding opencv stuff

--- HighlanderChef/src/com/highlanderchef/ImageComp.java
+++ HighlanderChef/src/com/highlanderchef/ImageComp.java
@@ -1,9 +1,12 @@
 package com.highlanderchef;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -15,7 +18,7 @@ import android.widget.FrameLayout;
 
 public class ImageComp extends ActionBarActivity
 {
-
+	private Bitmap image1;
 	private CameraPreview camPreview;
 	private FrameLayout mainLayout;
 
@@ -32,15 +35,19 @@ public class ImageComp extends ActionBarActivity
 		//requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_image_comp);
 
+		Intent intent = getIntent();
+		image1 = (Bitmap)intent.getParcelableExtra("image");
+
+		Display display = getWindowManager().getDefaultDisplay();
 		SurfaceView camView = new SurfaceView(this);
 		SurfaceHolder camHolder = camView.getHolder();
-		camPreview = new CameraPreview(640, 480);
+		camPreview = new CameraPreview(800, 480, image1);
 
 		camHolder.addCallback(camPreview);
 		camHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
 		mainLayout = (FrameLayout) findViewById(R.id.camera_preview);
-		mainLayout.addView(camView, new LayoutParams(640, 480));
+		mainLayout.addView(camView, new LayoutParams(800, 480));
 	}
 
 	@Override
